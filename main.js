723af1aef9f97d490ec89b181433600ca3e4aa95
André Santana <andresanlopes@gmail.com>
2023-03-08 12:11:02 -0300

    Esqueleto do projeto - 08/03

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+GLOBAIS
+*/
+
+var opcao1 = document.querySelector("a#item1")
+var opcao2 = document.querySelector("a#item2")
+var opcao3 = document.querySelector("a#item3")
+
+function grifar() {
+    opcao1.style.color = '#0e0e0e'
+}
+
+function grifar2(){
+    opcao2.style.color = '#0e0e0e'
+}
+
+function grifar3(){
+    opcao3.style.color = '#0e0e0e'
+}
+
+function opaco(){
+    opcao1.style.color = '#0e0e0eab'
+    opcao2.style.color = '#0e0e0eab'
+    opcao3.style.color = '#0e0e0eab'
+}
+
+/*var teste = document.querySelector("p#escreve")
+teste.innerHTML += `<br>TESTE 1, 2, 3`*/
